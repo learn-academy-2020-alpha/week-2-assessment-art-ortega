71f93f53f172d14762408aebd8ac91acdebe490f
Ubuntu <ubuntu@ip-172-31-20-245.us-east-2.compute.internal>
2020-02-15 00:22:25 +0000

    week2-assesment-art-ortega

--- code-challenges.js
+++ code-challenges.js
@@ -5,22 +5,32 @@
 
 var num1 = 15
 // Expected output: "15 is divisible by three"
-
 var num2 = 0
 // Expected output: "0 is divisible by three"
-
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
-
-
-
+const divBy3 = num =>{
+    if(num%3===0){
+        return `${num} is divisible by three`
+    } else{
+        return `${num} is not divisible by three`
+    }
+}
+console.log(divBy3(num1))
+console.log(divBy3(num2))
+console.log(divBy3(num3))
 
 // --------------------2) Create a function that takes in the randomNouns variable and returns an array with all the words capitalized.
 
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+const capitalized = array => {
+    let cap = array.map(value => value.replace(/^\w/, l => l.toUpperCase()))
+    return cap
+}
 
+console.log(capitalized(randomNouns))
 
 
 
@@ -30,8 +40,11 @@ var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "tem
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 90, 107]
 
-
-
+const numOnly = array =>{
+    let nums = array.filter(value=> typeof value === "number").sort((a,b)=> a-b)
+    return nums
+}
+console.log(numOnly(mixedDataArray))
 
 
 // --------------------4) Create a function that takes in a string and logs the index of the first vowel.
@@ -39,26 +52,56 @@ var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 
 var vowelTester1 = "learn"
 // Expected output: 1
-var vowelTester1 = "throw"
+var vowelTester2 = "t"
 // Expected output: 3
 
-
-
+const indexVowel = str => {
+    let vowels = "aeiou"
+    let count = 0
+   for(let i=0;i<str.length;i++){
+       if(vowels.includes(str.charAt(i))){
+           return i
+           count++
+       }
+   }
+   if(count<1){
+       return "String does not contain vowels"
+   }
+    
+}
+console.log(indexVowel(vowelTester1))
+console.log(indexVowel(vowelTester2))
 
 
 
 // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
 
 
+const calculator = (num1,operator,num2) =>{
+    if(operator === "*"){
+        return num1 * num2
+    } else if(operator ==="/"){
+       if(num2 === 0){
+           return `Can't divide by 0`
+       }
+        return num1 /num2
+    } else if(operator === "+"){
+        return num1 + num2
+    } else if(operator ==="-"){
+       return  num1 - num2
+    } else {
+        return `Make sure you enter an operator`
+    }
+}
 
 
 
 // Uncomment and use the following console logs to test your function
-// console.log(calculator(3, "*" 9))
+console.log(calculator(3, "*", 9))
 // Expected output: 27
 
-// console.log(calculator(16, "+" 3))
+console.log(calculator(16, "+", 3))
 // Expected output: 19
 
-// console.log(calculator(89, "/" 0))
+console.log(calculator(89, "/", 0))
 // Expected output: "Can't divide by 0!"
--- interview-questions.md
+++ interview-questions.md
@@ -4,49 +4,49 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What does CRUD stand for?
 
-  Your answer:
+  Your answer: Create, Read, Update, Delete
 
-  Researched answer:
+  Researched answer:In computer programming, create, read, update, and delete (CRUD) are the four basic functions of persistent storage. 
 
 
 
 2. What are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: Post, Put, Patch, Delete, 
 
-  Researched answer:
+  Researched answer: POST, GET, PUT, PATCH, and DELETE
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to render "Hello World" on the page?
 
-  Your answer:
+  Your answer: a render and return with jsx inside the return. there has to be a parent jsx tag (div) wraped around for it to work
 
-  Researched answer:
+  Researched answer: You need to import react and then use the render and return prior to the JSX. 
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: XML/HTML like syntax used by React
 
-  Researched answer:
+  Researched answer: It is a syntax extension to Javascript used by React. JSX produces react "elements".
 
 
 
 5. What is the difference between React state and props?
 
-  Your answer:
+  Your answer: Props are passed down to the child and can use the parents state. State holds the value of the state of the component 
 
-  Researched answer:
+  Researched answer: In a React component, props are variables passed to it by its parent component. State on the other hand is still variables, but directly initialized and managed by the component
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: moving stuff to the top within a javascript file
 
-  Researched answer:
+  Researched answer: Moves declaratiins to the top of a JS file so they can be used. ex: declaring let var; at the bottom will move it to the top of the page allowing the variable to be assigned before the decleration. 
 
 
 
@@ -54,7 +54,7 @@ Answer the following questions. First, without external resources. Challenge you
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods
-- API
-- event.preventDefault()
-- DOM events
+- React lifecycle methods:Each component in React has a lifecycle which you can monitor and manipulate during its three main phases. The phases are mounting, updating, and unmounting.
+- API:set of programming code that enables data transmission between one software product and another. It also contains the terms of this data exchange.
+- event.preventDefault():tells the user agent that if the event does not get explicitly handled, its default action should not be taken as it normally would be.
+- DOM events: Events that occur as a result of user action or as result of state change of the elements of a DOM tree.
